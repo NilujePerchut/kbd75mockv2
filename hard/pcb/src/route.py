6b25d2aa4cb3b48e10bd5251e02923d6a6779585
niluje <debarge.cedric@gmail.com>
2021-08-30 22:04:50 +0200

    Add routing script with edge cut

--- hard/pcb/src/route.py
+++ hard/pcb/src/route.py
@@ -3,31 +3,87 @@
 
 import pcbnew
 import argparse
-from pcbnew import FromMM
+from pcbnew import FromMM, FromMils
 
 from route_utils import get_layer_table
 
+SHEET_LEN = FromMM(410)
+SHEET_HIGHT = FromMM(210)
 
-def draw_edge_cut(pcb, fillet_radius=1, max_fillet_error=50):
-    """Draws the edge cut."""
+# Plate dimensions
+PLATE_X = FromMM(308)
+PLATE_Y = FromMM(118)
+
+# PCB dimensions
+PCB_X = FromMM(302)
+PCB_Y = FromMM(114.5)
+
+BOUNDING_POLY_COMPENTATION = FromMM(0.2)
+ARC_SEGMENT_COUNT = 50
+
+U1 = FromMM(19.05)
+
+# Case related constants
+UP_THICKNESS = FromMM(10)
+DOWN_THICKNESS = FromMM(3.8)
+UPPER_CASE_TO_FIRST_FIX = FromMM(19.66)
+
+# JLCPCB settings
+JLCPCB_CAPABILITIES = {
+    "TrackMinWidth": FromMils(3.5),
+    "ViasMinSize": FromMM(0.4),
+    "ViasMinDrill": FromMM(0.2),
+
+    "Clearance": FromMils(5),
+    "TrackWidth": FromMils(10),
+    "ViaDiameter": FromMils(27),
+    "ViaDrill": FromMils(13),
+    "uViaDiameter": FromMils(27),
+    "uViaDrill": FromMils(13),
+    "DiffPairWidth": FromMM(0.229),
+    "DiffPairGap": FromMM(0.154),
+}
+
+
+def _get_module_boudingpoly(pcb, switches=False, usb_conn=False):
+    """Return the outline of all the selected modules"""
     modules = []
-    edgecuts_layer = get_layer_table(pcb)["Edge.Cuts"]
-
     for module in pcb.GetModules():
         # Only consider Switches and USB connector for now
         lib_name = str(module.GetFPID().GetLibItemName())
-        if lib_name.startswith("SW_Cherry_MX") or lib_name.startswith("USB_C"):
+        if switches and lib_name.startswith("SW_Cherry_MX"):
+            modules.append(module)
+        elif usb_conn and lib_name.startswith("USB_C"):
             modules.append(module)
 
     hull = modules[0].GetBoundingPoly()
     for m in modules[1:]:
         hull.BooleanAdd(m.GetBoundingPoly(), hull.PM_FAST)
+    hull.Unfracture(hull.PM_FAST)
     hull.RemoveNullSegments()
-    hull = hull.Fillet(FromMM(fillet_radius), max_fillet_error)
+    # Compensate GetBoundingPoly
+    hull.Inflate(-BOUNDING_POLY_COMPENTATION, ARC_SEGMENT_COUNT)
+    return hull
 
-    hull_outline = hull.Outline(0)
-    points = [hull_outline.CPoint(i).getWxPoint()
-              for i in range(hull_outline.PointCount())]
+
+def _is_inside_sheet(point):
+    """Return True if given point is located inside the sheet"""
+    if point.x < 0:
+        return False
+    if point.x > SHEET_LEN:
+        return False
+    if point.y < 0:    
+        return False
+    if point.y > SHEET_HIGHT:    
+        return False
+    return True
+
+
+def _draw_tracks_from_outline(pcb, outline, layer):
+    """Draw track along an outline"""
+    points = [outline.CPoint(i).getWxPoint()
+              for i in range(outline.PointCount())]
+    points = [point for point in points if _is_inside_sheet(point)]
     points.append(points[0])
 
     p1 = points[0]
@@ -36,12 +92,42 @@ def draw_edge_cut(pcb, fillet_radius=1, max_fillet_error=50):
         pcb.Add(seg)
         seg.SetStart(p1)
         seg.SetEnd(p2)
-        seg.SetLayer(edgecuts_layer)
+        seg.SetLayer(layer)
         p1 = p2
+
+
+def setup_pcb_options(pcb, profile):
+    """Apply board options"""
+    design_settings = pcb.GetDesignSettings()
+    design_settings.SetCopperLayerCount(2)
+
+    design_settings.m_TrackMinWidth = profile["TrackMinWidth"]
+    design_settings.m_ViasMinSize = profile["ViasMinSize"]
+    design_settings.m_ViasMinDrill = profile["ViasMinDrill"]
+
+    default_class = design_settings.GetDefault()
+    default_class.SetClearance(profile["Clearance"])
+    default_class.SetTrackWidth(profile["TrackWidth"])
+    default_class.SetViaDiameter(profile["ViaDiameter"])
+    default_class.SetViaDrill(profile["ViaDrill"])
+    default_class.SetuViaDiameter(profile["uViaDiameter"])
+    default_class.SetuViaDrill(profile["ViaDrill"])
+    default_class.SetDiffPairWidth(profile["DiffPairWidth"])
+    default_class.SetDiffPairGap(profile["DiffPairGap"])
+
+
+def draw_edge_cut(pcb):
+    """Draws the edge cut."""
+    hull = _get_module_boudingpoly(pcb, switches=True, usb_conn=True)
+    outline = hull.Outline(0)
+    edgecuts_layer = get_layer_table(pcb)["Edge.Cuts"]
+    _draw_tracks_from_outline(pcb, outline, edgecuts_layer)
+
 
 def route(unrouted, routed):
     """Routes the (hopefuly) most of the pcb"""
     pcb = pcbnew.LoadBoard(unrouted)
+    setup_pcb_options(pcb, JLCPCB_CAPABILITIES)
     draw_edge_cut(pcb)
     pcb.Save(routed)
 
