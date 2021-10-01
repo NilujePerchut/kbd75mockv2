b9179af80089ff39ab164d884a5436da20d67e5c
niluje <debarge.cedric@gmail.com>
2021-10-01 21:32:47 +0200

    Use a bigger Sakura !

--- hard/pcb/src/mechs.py
+++ hard/pcb/src/mechs.py
@@ -4,6 +4,6 @@ def mechs():
     """Insert mechanicals stuff"""
     [dop_part("FIDUCIAL", "FIDUCIAL") for i in range(3)]
     [dop_part("Fix", "JLCPCB_TOOLING_HOLE") for i in range(3)] 
-    dop_part("Sakura", "Sakura")
+    dop_part("Sakura", "Sakura_big")
     [dop_part("Fix", "FIX_KEYBOARD") for i in range(10)]
     [dop_part("YMDK_case", "YMDK_case")]
